57a0f8dd53a1a3168af24b06ec09b2176f54d040
UnKnownVar <crazymanloganschool@gmail.com>
2017-01-21 16:12:07 -0500

    Getting IO

---
+++ src/org/gosparx/team1126/robot/subsystem/Climbing.java
@@ -0,0 +1,10 @@
+package org.gosparx.team1126.robot.subsystem;
+
+import org.gosparx.team1126.robot.IO;
+import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+
+
+public class Climbing extends GenericSubsystem {
+
+}
