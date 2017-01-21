683c61bc62c843a5d87cde50e5f7e05c26a274fd
nphto <nphto>
2017-01-21 11:38:14 -0500

    framework cont.

--- src/org/gosparx/team1126/robot/subsystem/Shooter.java
+++ src/org/gosparx/team1126/robot/subsystem/Shooter.java
@@ -1,11 +1,97 @@
 package org.gosparx.team1126.robot.subsystem;
 
+import edu.wpi.first.wpilibj.Encoder;
+import org.gosparx.team1126.robot.sensors.EncoderData;
+
 public class Shooter extends GenericSubsystem{
 
+//*****************************************Variables*************************************\\
+	
 	/**
-	 * the instance of a Shooter object
+	 * true if first agitator is moving
 	 */
+	private boolean firstAgitatorMoving;
+	
+	/**
+	 * true if second agitator is moving
+	 */
+	private boolean secondAgitatorMoving;
+	
+	/**
+	 * true if conveyer belt is moving
+	 */
+	private boolean conveyerBeltMoving;
+	
+	/**
+	 * true if shooting
+	 */
+	private boolean shooting;
+	
+	/**
+	 * current shooting speed;
+	 */
+	private double shootingSpeedCurrent;
+	
+	/**
+	 * the speed wanted before shooting
+	 */
+	private double shootingSpeed;
+	
+	/**
+	 * current turret degree
+	 */
+	private double turretDegree;
+	
+	/**
+	 * if the turret motor is moving 
+	 */
+	private boolean turretMotorMoving;
+	
+	/**
+	 * current motor speed 
+	 */
+	private double turretMotorCurrent;
+	
+//******************************************Objects**************************************\\
+	
 	private static Shooter shoot;
+	
+	private Encoder encoder;
+	
+	private EncoderData encoderData;
+	
+	private AbsoluteEncoderData turretSensor;
+	
+//*****************************************Constants*************************************\\
+	/**
+	 * the speed wanted for the first agitator 
+	 */
+	private final double firstAgitatorSpeed = 0;
+	
+	/**
+	 * The speed wanted for the second agitator;
+	 */
+	private final double secondAgitatorSpeed = 0;
+	
+	/**
+	 * The speed wanted for the conveyer belt
+	 */
+	private final double conveyerBeltSpeed = 0;
+	
+	/**
+	 * the case number for shooting
+	 */
+	private final int caseShooting = 1;
+	
+	/**
+	 * the case number for the turret
+	 */
+	private final int caseTurret = 2;
+	
+	
+	
+	
+	
 	
 	/**
 	 * Constructs a shooter object
@@ -23,8 +109,15 @@ public class Shooter extends GenericSubsystem{
 	
 	@Override
 	protected boolean init() {
-		
-		return false;
+		turretSensor = new AbsoluteEncoderData();
+		firstAgitatorMoving = false;
+		secondAgitatorMoving = false;
+		conveyerBeltMoving = false;
+		turretMotorMoving  = false;
+		shootingSpeedCurrent = 0;
+		turretDegree = 0;
+		turretMotorCurrent = 0;
+		return true;
 	}
 
 	@Override
@@ -34,7 +127,28 @@ public class Shooter extends GenericSubsystem{
 
 	@Override
 	protected boolean execute() {
+		int option = 1;
 		
+		
+		switch(option){
+		case caseShooting:
+			if(shooting == false){
+				shooting = true;
+			}	
+			else if(shootingSpeedCurrent < shootingSpeed){
+				
+			}
+			break;
+		case caseTurret:
+			if(turretDegree > 360){
+				turretDegree -= 360;
+			}else if(turretDegree < 0){
+				turretDegree += 360;
+			}
+			if(turretMotorMoving == false){
+				turretMotorMoving = true;
+			}
+		}
 		return false;
 	}
 
