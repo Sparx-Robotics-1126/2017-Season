d90d689e7147cdaaefc1dbcb00c7fee6f967abc9
UnKnownVar <crazymanloganschool@gmail.com>
2017-01-30 16:50:32 -0500

    Finished climbing for now. yay!

--- src/org/gosparx/team1126/robot/subsystem/Climbing.java
+++ src/org/gosparx/team1126/robot/subsystem/Climbing.java
@@ -4,15 +4,40 @@ import java.security.InvalidParameterException;
 
 import org.gosparx.team1126.robot.util.Logger;
 
+import com.ctre.CANTalon;
+
+import edu.wpi.first.wpilibj.DigitalInput;
+
 import edu.wpi.first.wpilibj.DriverStation;
+
 import org.gosparx.team1126.robot.IO;
+
 import edu.wpi.first.wpilibj.Timer;
+
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+
+//import org.gosparx.team1126.robot.subsystem.Drives;
 
 
 public class Climbing extends GenericSubsystem {
 
 	private State currentClimbingStatus;
+	
+	private static Climbing climbing;
+	
+	private static final double MOTOR_SPIN = 1.0;
+	
+	private static final double MOTOR_STOP = 0;
+	
+	private static final double MOTOR_ATTATCH = 0.5;
+	
+	private DigitalInput ClimbingSensor;
+	
+	private CANTalon ClimbingMotor;
+	
+	private static int MotorSpeed = 0;
+	
+	//private Drives drives;
 	
 	public Climbing(String name, int priority){
 		super(name, priority);
@@ -44,8 +69,38 @@ public class Climbing extends GenericSubsystem {
     }
 
 	@Override
+	protected boolean execute() {
+		switch(currentClimbingStatus){
+		case STANDBY:{
+			ClimbingMotor.set(MOTOR_STOP);
+			
+			break;
+		}
+		case ATTATCHING:{
+			ClimbingMotor.set(MOTOR_ATTATCH);
+			currentClimbingStatus = State.CLIMBING;
+			
+			break;
+		}
+		case CLIMBING:	
+			ClimbingMotor.set(MOTOR_SPIN);
+			if(ClimbingSensor.get() == true){
+				LOG.logMessage("Rope Has Been Climbed");
+				currentClimbingStatus = State.STANDBY;
+			}
+			break;
+		default:
+			break;
+			}
+		return false;		
+	}
+	
+	
+	@Override
 	protected boolean init() {
-		// TODO Auto-generated method stub
+		ClimbingSensor = new DigitalInput(IO.DIO_CLIMBING_LIMITSWITCH);
+		
+		ClimbingMotor = new CANTalon(IO.CAN_CLIMBING);
 		return false;
 	}
 
@@ -55,11 +110,7 @@ public class Climbing extends GenericSubsystem {
 		
 	}
 
-	@Override
-	protected boolean execute() {
-		// TODO Auto-generated method stub
-		return false;
-	}
+
 
 	@Override
 	protected long sleepTime() {
