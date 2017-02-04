b95cff737774e7818b6079ef567a5449be38a00b
MJB <Me@10.97.33.38>
2017-02-04 15:33:03 -0500

    Configured "empty" subsystem

--- src/org/gosparx/team1126/robot/subsystem/TestSubsystem.java
+++ src/org/gosparx/team1126/robot/subsystem/TestSubsystem.java
@@ -1,5 +1,57 @@
 package org.gosparx.team1126.robot.subsystem;
 
-public class TestSubsystem {
+public class TestSubsystem extends GenericSubsystem {
+	
+	private static TestSubsystem test;
+	
+	/**
+	 * Constructors a drives object with normal priority
+	 */
+	private TestSubsystem(){
+		super("Test",Thread.NORM_PRIORITY);
+	}
 
+	/**
+	 * ensures that there is only one instance of drives		
+	 * @return the instance of drives 
+	 */
+	public static synchronized TestSubsystem getInstance(){
+		if(test == null){
+			test = new TestSubsystem();
+		}
+		return test;											
+	}
+
+	@Override
+	protected void liveWindow() {
+	}
+
+	@Override
+	protected boolean execute() {
+		return true;
+	}
+
+	/**
+	 * Instantiates all the objects and initializes the variables  
+	 * @return true if it runs once, false if it continues, should return true
+	 */
+	@Override
+	protected boolean init(){
+		return true;
+	}
+	/**
+	 * the time the system "sleeps" until it is called again
+	 * @return the time in milliseconds
+	 */
+	@Override
+	protected long sleepTime() {
+		return 20;
+	}
+
+	/**
+	 * Writes logs to the console every 5 seconds
+	 */
+	@Override
+	protected void writeLog() {
+	}
 }
