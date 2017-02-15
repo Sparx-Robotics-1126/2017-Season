2c334eb6d96d17ac67ad7cf7f26ec759df9fc20c
MJB <Me@10.97.33.38>
2017-02-15 14:45:20 -0500

    Added getter (real word?) functions for internal variable retrieval

--- src/org/gosparx/team1126/robot/sensors/PID.java
+++ src/org/gosparx/team1126/robot/sensors/PID.java
@@ -1,35 +1,40 @@
 package org.gosparx.team1126.robot.sensors;
 
-import edu.wpi.first.wpilibj.DriverStation;
-
 /**
- * Logic of a Proportional Integral Derivative loop. Must be constructed first,
- * then it must receive continual updates in order to receive accurate output
- * values.
+ * Logic of a Proportional Integral Derivative loop. Must be constructed first, then it must receive
+ * periodic calls to the "loop" function in order to calculate the appropriate output values.
+ * 
+ * 	Derivative calculation not yet available
  */
 
 public class PID {
 	
-	private double kp;							        // Proportional Constant
-	private double ki;						 	        // Integral Constant
+	private double kp = 0;						        // Proportional Constant
+	private double ki = 0;					 	        // Integral Constant
 	private double kf = 0;						        // Linear Feedforward Constant
-	private double error = 0;					        // Delta between setpoint and PV
 	private double integral = 0;					    // Integral portion of the output
 	private double proportional = 0;				    // Proportional portion of the output
 	private double feedForward = 0;				        // Feedforward portion of the output
 	private double totalizer = 0;				        // Cumulative sum of the error for integral calc
-	private double currentTime = 0;				        // Current time (in seconds)
 	private double outMax = 1.0;						// Default Maximum Output
 	private double outMin = -1.0;						// Default Minimum Output
 	private double pastTime = 0;					    // Last time the loop was run (in seconds)
 	private double spRampLimit = 0;						// Setpoint Ramp Limit
 	private double internalSP = 0;						// Internal Setpoint
+	private double loopTimeLimit = 0.1;					// Maximum limit for loop timing
 	private boolean spRamping = false;					// Setpoint ramping
 	private boolean outMaxMinEna = false;				// Output Max/Min Enabled
 	private boolean stopFunction = false;		        // Use speed controller brake mode flag
 
 	/* PID Constructors */
 	
+	public PID(){
+	}
+	
+	public PID(double kP){
+		kp = kP;
+	}
+
 	public PID(double kI, double kP){
 		ki = kI;
 		kp = kP;
@@ -50,7 +55,9 @@ public class PID {
 	
 	public double loop(double speed, double setPoint){
 
+		double currentTime;						        // Current time (in seconds)
 		double elapsedTime;								// Elapsed time since last call
+		double error;									// Error between SP & PV
 		double output;									// Calculated Output
 		
 		// Acquire the current time (in milliseconds), convert to seconds and calculate the
@@ -66,25 +73,31 @@ public class PID {
 		// stopFunction flag, and the desired setpoint is zero, then immediately return a zero output.
 		// Setting the speed controller to zero (0), will cause it to enter brake mode if set.  Zero
 		// the cumulative error term so that the next time a non-zero setpoint is used, the loop doesn't
-		// use an old I component.
+		// use an old I component as well as update the internal setpoint immediately.
 		
-		if((setPoint == 0) && (stopFunction == true)){
+		if((setPoint == 0) && (stopFunction == true))
+		{
+			internalSP = 0;
 			totalizer = 0;
+			proportional = 0;							// Clear internal variables in case an external
+			integral = 0;								//	call is made to print/record these values.
+			feedForward = 0;
 			return 0;
 		}
 		
-		if(elapsedTime > .1)							// Limit the maximum elapsed time to 0.1 sec
-			elapsedTime = .1;
+		if(elapsedTime > loopTimeLimit)					// Limit the maximum elapsed time
+			elapsedTime = loopTimeLimit;
 
 		if ((spRamping == true) &&						// Check for SP Ramping enabled,
-				(Math.abs(setPoint - internalSP) > 		// and over the limit
-				(spRampLimit * elapsedTime)))
+			(Math.abs(setPoint - internalSP) >	 		// and over the limit
+			(spRampLimit * elapsedTime)))
 		{
 			if (setPoint - internalSP > 0)				// Are we ramping UP?
 				internalSP += spRampLimit * elapsedTime;// Add the maximum ramp limit
 			else
 				internalSP -= spRampLimit * elapsedTime;// Else subtract the maximum limit
-		}else
+		}
+		else
 			internalSP = setPoint;
 		
 		error = internalSP - speed;						// Calculate the error between the setpoint and PV
@@ -95,22 +108,27 @@ public class PID {
 		
 		output = proportional + integral + feedForward;	// Calculate output valule
 		
-		if (outMaxMinEna == true){						// Is Min/Max limiting enabled?
-			if (output > outMax){						// Are we above the maximum output value?
+		if (outMaxMinEna == true)						// Is Min/Max limiting enabled?
+		{
+			if (output > outMax)						// Are we above the maximum output value?
+			{
 				if (integral > output - outMax)			// Is compensating with i term possible?
 					totalizer -= (output - outMax) / ki;// Adjust i term to the maximum limit;
 				else if (integral > 0)					// Otherwise the i term must be 0 if positive
 					totalizer = 0;
 				
 				output = outMax;						// Set output to max limit
+				integral = ki * totalizer;				// Correct integral component for retrieval
 			}
-			if (output < outMin){						// Are we less than the minimum value?
+			if (output < outMin)						// Are we less than the minimum value?
+			{
 				if (integral < output - outMin)			// Is compensating with the i term possible?
 					totalizer -= (output - outMin) / ki;// Adjust i term to maximum (negative) value
 				else if (integral < 0)					// Otherwise the i term must be 0 if negative
 					totalizer = 0;
 				
 				output = outMin;
+				integral = ki * totalizer;				// Correct integral component for retrieval
 			}
 		}		
 		return (output);								// returns output power
@@ -148,10 +166,50 @@ public class PID {
 		spRampLimit = ramp;
 	}
 	
+	/* Enable the changing of the maximum time between loop executions.  By default, this is set to 0.1 sec.
+	 * so that any single loop won't have a dramatic effect on the output should the thread get dealyed by
+	 * something else occurring in the processor (blocking).
+	 */
+	
+	public void setMaxLoopTime ( double maxTime ){
+		loopTimeLimit = maxTime;
+	}
+	
 	/* Routine to allow for changes/updates to the PID constants */
 	
-    public void pidconstants (double newkp, double newki){
+    public void pidconstants (double newkp, double newki, double newkf){
     	kp = newkp;
     	ki = newki;
+    	kf = newkf;
+    }
+ 
+    /* Routines to retrieve internal variables for logging/plotting/tracking/printing */
+    
+    public double getProportional(){
+    	return proportional;
+    }
+    
+    public double getIntegral(){
+    	return integral;
+    }
+    
+    public double getFeedforward(){
+    	return feedForward;
+    }
+    
+    public double getInternalSP(){
+    	return internalSP;
+    }
+    
+    public double getki(){
+    	return ki;
+    }
+    
+    public double getkp(){
+    	return kp;
+    }
+    
+    public double getkf(){
+    	return kf;
     }
 }
