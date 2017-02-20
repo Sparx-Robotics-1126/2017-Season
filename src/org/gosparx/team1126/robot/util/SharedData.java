8a6e7d6e5bb8c5e7f4d1048f2ba482b838aa0cb6
secretspy97 <nathan.connor@gosparx.org>
2017-02-20 14:01:22 -0500

    updated

---
+++ src/org/gosparx/team1126/robot/util/SharedData.java
@@ -0,0 +1,59 @@
+package org.gosparx.team1126.robot.util;
+
+public class SharedData {
+
+	// Drives Subsystem Shared Data
+
+	public static double x;
+	public static double y;
+	public static double heading;
+	public static double leftSpeed;
+	public static double rightSpeed;
+	public static double avgSpeed;
+
+
+	// Shooter & Turret Subsystem Shared Data
+
+	public static double turretAngle;
+	public static double shooterSpeed;
+	public static boolean systemReady;
+
+
+	// Camera SubSystem Shared Data
+
+	public enum Target { BOILER, LIFT };
+
+	public static double distanceToBoiler;
+	public static double angleToBoiler;
+	//public static double distanceToLift; //For if there is no ultra sonic sound sensor 
+	public static double angleToLift;
+	public static Target targetType;
+	private static double targetXBoiler, targetYBoiler; // (X, Y) at image time
+	//private static double targetXLift, targetYLift;
+	private static long imageTime;						// Time of Image
+
+	public static void setTarget (Target type, double distance, double angle){
+		if(type==Target.BOILER){
+			distanceToBoiler = distance;
+			angleToBoiler = angle;
+			targetXBoiler = x + Math.sin(Math.toRadians(heading + angleToBoiler)) * distanceToBoiler;
+			targetYBoiler = y + Math.cos(Math.toRadians(heading + angleToBoiler)) * distanceToBoiler;
+		}
+		else
+		{
+			angleToLift = angle;
+//			distanceToLift=distance;
+//			targetXLift = x + Math.sin(Math.toRadians(heading + angleToLift)) * distanceToLift;
+//			targetYLift = y + Math.cos(Math.toRadians(heading + angleToLift)) * distanceToLift;
+		}
+		imageTime = System.currentTimeMillis();
+	}
+
+	public static double getCorrectedTargetAngle(){
+		return(Math.IEEEremainder((Math.atan2(targetXBoiler - x, targetYBoiler - y) - heading), 360.0));
+	}
+
+	public static double getCorrectedTargetDistance(){
+		return (Math.sqrt(Math.pow(targetXBoiler - x, 2) + Math.pow(targetYBoiler - y, 2)));
+	}
+}
--- src/org/gosparx/team1126/robot/util/visionNetworkTable.java
+++ src/org/gosparx/team1126/robot/util/visionNetworkTable.java
@@ -3,142 +3,90 @@ import edu.wpi.first.wpilibj.networktables.NetworkTable;
 import edu.wpi.first.wpilibj.tables.ITable;
 import edu.wpi.first.wpilibj.tables.ITableListener;
 
-
 //import edu.wpi.first.wpilibj.vision.VisionRunner.Listener;
 /*########################################################################*/	
 public class visionNetworkTable implements ITableListener{
-/*########################################################################*/	
-private static NetworkTable client;
-private NetworkTable server;
-private String IP = "10.11.26.20"; //Jetsons boards IP
-//private int port = 1735; //Jenson's board port
-private String serverKey = "mode"; //For sending mode
-private String clientLift = "peg"; //For reciving angle and distance for lift
-private String clientHighGoal = "highGoal"; ////For reciving angle and distance for HighGoal 
-private double angle; //To hold angle
-private double distance; //To hold distance
-private SharedData.Target currentMode; //BOILER or LIFT //0 is off, 1 is Highgoal, 2 is lift
-double[] arrGoalLift;
-
-/*########################################################################*/	
-//For testing (may need to make some variables static)
-//public static void main(String[] args)
-//{
-//	
-//	visionNetworkTable instance = new visionNetworkTable();
-//	while (true)
-//	{
-//	instance.serverUpdate();
-//	break;
-//	}
-//	//double[] d = {2.0,2.0};
-//	//instance.valueChanged(client, clientHighGoal, d, true);
-//	
-//	}
-/*########################################################################*/
-public visionNetworkTable() //Constructor
-{
-	NetworkTable.setClientMode();
-	NetworkTable.setIPAddress(IP); //Sets Ip address
-	client = NetworkTable.getTable("targetData"); //Gets client table 
-
-	//Adds the listener to see if value changed
-	client.addTableListener(this, true);
+	/*########################################################################*/	
+	private static NetworkTable client;
 	
-	//NetworkTable.setServerMode(); //Sets to Server mode  
-	server = NetworkTable.getTable(serverKey); //Makes server table
-	//serverRun(); //To make sure the Table has a value
-}
-
-/*#####################################################################################*/	
-private void serverUpdate() //For sending mode
-{
-	currentMode = SharedData.Target.BOILER;
-//	if((currentMode!=lastMode)) //If I already sent current mode then it does not send
-//	{
-//		
-		server.putValue(serverKey, new Boolean(currentMode == SharedData.Target.LIFT)); //Puts the mode in table
-//		lastMode = currentMode;
-		//System.out.println("Mode:"+currentMode); //Prints the mode for debug
-//	}
-	try{Thread.sleep(5);} //Pauses for 5 mili
-	catch(Exception e)
+	private String IP = "10.11.26.60"; //Jetsons boards IP
+	//private int port = 1735; //Jenson's board port (might not need)
+	private String serverKey = "mode"; //For sending mode
+	private static String clientLift = "peg"; //For reciving angle and distance for lift
+	private static String clientHighGoal = "highGoal"; ////For reciving angle and distance for HighGoal 
+	private SharedData.Target currentMode; //BOILER or LIFT //0 is off, 1 is Highgoal, 2 is lift
+	double[] arrTargetData;
+	/*########################################################################*/	
+	public static void main(String[] args)
 	{
-		System.out.println("\n"+"InterruptedException exception at run"); //If somthing went wrong
-		System.out.println(e.getMessage());
+		visionNetworkTable instance = new visionNetworkTable();
+		instance.serverUpdate();
+		double[] d = {2.0,2.0};
+		instance.valueChanged(client, clientHighGoal, d, true);
 	}
-}
-/*#####################################################################################*/	
-@Override //For listner 
-public void valueChanged(ITable itable, String Values_Key, Object val, boolean bln)
-{
-	try
+
+
+	/*########################################################################*/
+	public visionNetworkTable() //Constructor
 	{
-		arrGoalLift = (double[]) val; 
-		if (Values_Key.equals(clientHighGoal) && currentMode == SharedData.Target.BOILER) //If it has the right key
-		{
-			//Gets the angle and distance for HighGoal
-			 
-			angle = arrGoalLift[0];
-			distance = arrGoalLift[1];
-			System.out.println("angle and distance: "+angle+" "+distance+"\n");	
-			SharedData.angleToTarget = angle;
-			SharedData.distanceToTarget = distance;
-			
-			try{Thread.sleep(5); System.out.println("Sleeping");}	
-		    catch(InterruptedException e)
+		currentMode = SharedData.Target.BOILER;
+		NetworkTable.setClientMode();
+		NetworkTable.setIPAddress(IP); //Sets Ip address
+		client = NetworkTable.getTable("targetData"); //Gets client table 
+
+		//Adds the listener to see if value changed
+		client.addTableListener(this, true);
+	}
+
+	/*#####################################################################################*/	
+	private void serverUpdate() //For sending mode
+	{
+		try{	 
+			currentMode = SharedData.targetType;		
+			client.putValue(serverKey, new Boolean(currentMode == SharedData.Target.LIFT)); //Puts the mode in table
+			System.out.println("Mode:"+currentMode); //Prints the mode for debug
+
+			try{Thread.sleep(5);} //Pauses for 5 mili
+			catch(Exception e)
 			{
-			 System.out.println("\n"+"InterruptedException exception at run"+"\n");
-			 System.out.println(e.getMessage()+"\n");
-			}
-		}
-		else if(Values_Key == clientLift  && currentMode == SharedData.Target.LIFT) 
-		{
-			//Finds angle and distance for Lift
-			
-			
-			angle = arrGoalLift[0];
-			distance = arrGoalLift[1];
-			System.out.println(angle+" "+distance+"\n");	
-			SharedData.setTarget(currentMode, distance, angle);
-			SharedData.angleToTarget = angle;
-			SharedData.distanceToTarget = distance;
-			
-			///Shooter.update(angle,distance);
-			
-			try{Thread.sleep(5); System.out.println("Sleeping");}	
-		    catch(InterruptedException e)
-			{
-			 System.out.println("\n"+"InterruptedException exception at run"+"\n");
-			 System.out.println(e.getMessage()+"\n");
-			}
-	      
-			//System.out.print(val); //Prints info
-		}
-		else
-		{
-			System.out.println("Somthing Wrong");
-		} 
+				System.out.println("\n"+"Sleep problem??"); //If somthing went wrong
+				System.out.println(e.getMessage());
+			}}catch(Exception e){System.out.print("connection error");}
 	}
-	catch (Exception e)
+	/*#####################################################################################*/	
+	@Override //For listener 
+	public void valueChanged(ITable itable, String Values_Key, Object val, boolean bln)
 	{
-		System.out.println("\n"+"Error, value changed: "+Values_Key+" "+bln+" "+val+"\n");
-		System.out.println(e.getMessage());
-		// Print an error.
-	}
-}	
+		System.out.println("Change detected: "+val.getClass()+" at "+Values_Key+"\n Itable"+itable);
+		try //Catch exection e
+		{
+			arrTargetData = (double[]) val; 
+			if(Values_Key.equals(clientLift) || Values_Key.equals(clientHighGoal)) 
+			{
+				SharedData.setTarget(currentMode, arrTargetData[1], arrTargetData[0]);
+				System.out.println("Angle and distance: "+arrTargetData[0]+", "+arrTargetData[1]+"\n");	
+
+				try{Thread.sleep(5); System.out.println("Sleeping");} //Pauses	
+				catch(InterruptedException e)
+				{
+					System.out.println("\n"+"InterruptedException exception at run"+"\n");
+					System.out.println(e.getMessage()+"\n");
+				}
+
+				//System.out.print(val); //Prints info
+			}
+			else
+			{
+				System.out.println("Invalid key taken");
+			} 
+		}
+
+		catch (Exception e)
+		{
+			System.out.println("\n"+"Error, value changed: "+Values_Key+" "+bln+" "+val+"\n");
+			System.out.println(e.getMessage());
+			// Print an error.
+		}
+	}	
 /*#####################################################################################*/	
-//public void update(Boolean mode) //From nate
-//{
-//	if(mode!=null) //If true/false
-//		{
-//		mode = currentMode;
-//		serverUpdate(); //Sends the value
-//	}
-//}
-
 }	
-
-
-
-
