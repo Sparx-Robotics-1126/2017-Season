73c6d1210c9ce6409481b248c6b167ce4a3e66c5
nphto <nphto>
2017-03-13 18:04:01 -0400

    Updated IO

--- src/org/gosparx/team1126/robot/IO.java
+++ src/org/gosparx/team1126/robot/IO.java
@@ -12,134 +12,81 @@ public class IO {
 
 	/**************************************PWM*****************************************/
 	
-	public static final int PWM_BALLACQ_SERVO_AGITATOR						= 14;
-	
-	public static final int PWM_BALLACQ_SERVO_FUELGATE  					= 15;
+	public static final int PWM_BALLACQ_SERVO_AGITATOR					= 14;
+	public static final int PWM_BALLACQ_SERVO_FUELGATE  				= 15;
 	
 	/************************************DIO*******************************************/
 	
-	public static final int DIO_RIGHT_DRIVES_ENC_A                          = 10;
-
-	public static final int DIO_RIGHT_DRIVES_ENC_B                          = 11;
-	
-	public static final int DIO_LEFT_DRIVES_ENC_A                           = 12;
-
-	public static final int DIO_LEFT_DRIVES_ENC_B	                        = 13;
-	
-	public static final int DIO_GEARACQ_SENSOR								= 14;
-	
-	public static final int DIO_SHOOTER_ABS_ENC								= 15;
-	
-	public static final int DIO_CLIMBING_LIMITSWITCH						= 16;
-	
-	public static final int DIO_JETSON_RESET								= 17;
-	
-	public static final int DIO_SHOOTER_LIMITSWITCH_RIGHT					= 20;
-	
-	public static final int DIO_SHOOTER_LIMITSWITCH_LEFT					= 21;
-	
-	public static final int DIO_SHOOTER_ENC_A                               = 22;
-	
-	public static final int DIO_SHOOTER_ENC_B								= 23;
+	public static final int DIO_RIGHT_DRIVES_ENC_A                      = 0;
+	public static final int DIO_RIGHT_DRIVES_ENC_B                      = 1;
+	public static final int DIO_LEFT_DRIVES_ENC_A                       = 12;
+	public static final int DIO_LEFT_DRIVES_ENC_B	                    = 13;
+	public static final int DIO_GEARACQ_SENSOR							= 14;
+	public static final int ANALOG_SHOOTER_ABS_ENC						= 7;
+	public static final int DIO_CLIMBING_LIMITSWITCH					= 16;
+	public static final int DIO_JETSON_RESET							= 17;
+	public static final int DIO_SHOOTER_LIMITSWITCH_RIGHT				= 20;
+	public static final int DIO_SHOOTER_LIMITSWITCH_LEFT				= 21;
+	public static final int DIO_SHOOTER_ENC_A                           = 22;
+	public static final int DIO_SHOOTER_ENC_B							= 23;
 	
 	/**********************************CAN********************************************/
+	
+	//public static final int CAN_BALLACQ_RIGHT							= 1;
+	public static final int CAN_SHOOTER_TURRET							= 2;
+	public static final int CAN_CLIMBING_WINCH	   						= 3;
+	public static final int CAN_DRIVES_RIGHT_BACK                       = 4;
+	public static final int CAN_DRIVES_RIGHT_FRONT                      = 5;
+	//public static final int CAN_DRIVES_RIGHT_TOP						= 6;
+	public static final int CAN_SHOOTER_INTAKE_FEEDER					= 7;
+	public static final int CAN_BALLACQ_LEFT							= 8;
+	public static final int CAN_SHOOTER_FLYWHEEL                        = 9;
+	public static final int CAN_DRIVES_LEFT_BACK                        = 10;
+	public static final int CAN_DRIVES_LEFT_FRONT                       = 11;
+	//public static final int CAN_DRIVES_LEFT_TOP						= 12;
+	public static final int CAN_HOPPER_HORIZONTAL_BELT                  = 12;
 
-	/*
-	//CAN 2016 - Redefined for execution on the 2016 robot base
+	/**********************************USB Joysticks**********************************/
 	
-	public static final int CAN_DRIVES_LEFT_FRONT                           = 1;
-	public static final int CAN_DRIVES_LEFT_BACK                            = 2;
-	public static final int CAN_DRIVES_LEFT_TOP								= 4;	// Old Acquisition Shoulder
-	public static final int CAN_DRIVES_RIGHT_FRONT                          = 9;
-	public static final int CAN_DRIVES_RIGHT_BACK                           = 8;
-	public static final int CAN_DRIVES_RIGHT_TOP							= 5;	// Old Acquisition Shoulder 
-	 */
-	
-	// CAN 2017 - New definitions for 2017 robot
-	
-	public static final int CAN_BALLACQ_RIGHT								= 1;
-	
-	public static final int CAN_SHOOTER_TURRET								= 2;
-	
-	public static final int CAN_CLIMBING_WINCH	   							= 3;
-	
-	public static final int CAN_DRIVES_RIGHT_BACK                           = 4;
-	
-	public static final int CAN_DRIVES_RIGHT_FRONT                          = 5;
-	
-	public static final int CAN_DRIVES_RIGHT_TOP							= 6;
-	
-	public static final int CAN_SHOOTER_INTAKE_FEEDER						= 7;
-	
-	public static final int CAN_BALLACQ_LEFT								= 8;
-	
-	public static final int CAN_SHOOTER_FLYWHEEL                            = 9;
-	
-	public static final int CAN_DRIVES_LEFT_BACK                            = 10;
-	
-	public static final int CAN_DRIVES_LEFT_FRONT                           = 11;
-	
-	public static final int CAN_DRIVES_LEFT_TOP								= 12;
-
-	/**********************************USB********************************************/
-	
-	public static final int USB_DRIVER_LEFT                                 = 0;
-	
-	public static final int USB_DRIVER_RIGHT                                = 1;
-	
-	public static final int USB_OPERATOR                                    = 2;
+	public static final int USB_DRIVER_LEFT                             = 0;
+	public static final int USB_DRIVER_RIGHT                            = 1;
+	public static final int USB_OPERATOR                                = 2;
 	
 	/*********************************CAMS********************************************/
 	
-	public static final String[] CAMS                                       = {
-			                                                                "cam0",
-			                                                                "cam1"
-	                                                                        };
+	public static final String[] CAMS = {"cam0", "cam1"};
 	
 	/***********************BUTTONS/JOYSTICK******************************************/
 	
-	public static final int AUTO_TURN										= DriverStationControls.LEFT_JOY_TRIGGER;
+	// Drives
 	
-	public static final int AUTO_DRIVE										= DriverStationControls.RIGHT_JOY_LEFT;
-	
-	public static final int RESET_ENCODER									= DriverStationControls.LEFT_JOY_RIGHT;
+	public static final int AUTO_TURN									= DriverStationControls.LEFT_JOY_TRIGGER;
+	public static final int AUTO_DRIVE									= DriverStationControls.RIGHT_JOY_LEFT;
+	public static final int RESET_ENCODER								= DriverStationControls.LEFT_JOY_RIGHT;
+	public static final int ABORT_AUTO_DRIVES							= DriverStationControls.LEFT_JOY_LEFT;
+	public static final int INVERT_DRIVES_BUTTON						= DriverStationControls.RIGHT_JOY_TRIGGER;
+	public static final int HOLD_DRIVES									= DriverStationControls.RIGHT_JOY_RIGHT;
 
-	public static final int ABORT_AUTO_DRIVES								= DriverStationControls.LEFT_JOY_LEFT;
+	// Shooter
+	
+	public static final int FLIP_SHOOTING_SYSTEM_ON						= DriverStationControls.LEFT_JOY_MIDDLE;
+	public static final int FLIP_TARGET_LIFT							= DriverStationControls.OP_XBOX_L2;
+	public static final int BUTTON_FIRE									= DriverStationControls.OP_XBOX_A;
+	public static final int FIRE_OVERRIDE								= DriverStationControls.OP_XBOX_Y;
+	public static final int SCALING_RT									= DriverStationControls.OP_XBOX_R2;
+	public static final int RIGHT_JOY_X									= DriverStationControls.RIGHT_JOY_X_AXIS;
+	public static final int RIGHT_JOY_Y									= DriverStationControls.RIGHT_JOY_Y_AXIS;
+	public static final int LEFT_JOY_X									= DriverStationControls.LEFT_JOY_X_AXIS;
+	public static final int LEFT_JOY_Y									= DriverStationControls.LEFT_JOY_Y_AXIS;
+	public static final int TURRET_JOY_Y								= DriverStationControls.OP_XBOX_LEFT_X;
+	public static final int DIAGNOSTICS									= DriverStationControls.OP_XBOX_BACK;
+	public static final int FLYWHEEL_INCREASE						    = DriverStationControls.OP_XBOX_X;
+	public static final int FLYWHEEL_DECREASE 							= DriverStationControls.OP_XBOX_B;
 
-	public static final int INVERT_DRIVES_BUTTON							= DriverStationControls.RIGHT_JOY_TRIGGER;
+	// Acquisition & Shooting Belts
 	
-	public static final int HOLD_DRIVES										= DriverStationControls.RIGHT_JOY_RIGHT;
-	
-	public static final int FLIP_SHOOTING_SYSTEM_ON							= DriverStationControls.LEFT_JOY_MIDDLE;
-	
-	public static final int FLIP_TARGET_LIFT								= DriverStationControls.OP_XBOX_L2;
-	
-	public static final int BUTTON_FIRE										= DriverStationControls.OP_XBOX_A;
-	
-	public static final int SCALING_RT										= DriverStationControls.OP_XBOX_R2;
-	
-	public static final int RIGHT_JOY_X										= DriverStationControls.RIGHT_JOY_X_AXIS;
-	
-	public static final int RIGHT_JOY_Y										= DriverStationControls.RIGHT_JOY_Y_AXIS;
-	
-	public static final int LEFT_JOY_X										= DriverStationControls.LEFT_JOY_X_AXIS;
-	
-	public static final int LEFT_JOY_Y										= DriverStationControls.LEFT_JOY_Y_AXIS;
-	
-	public static final int DIAGNOSTICS										= DriverStationControls.OP_XBOX_BACK;
-	
-	public static final int ACQ_ON											= 0;
-	
-	public static final int ACQ_OFF											= 4;
-	
-	public static final int ACQ_FOWARD										= 2;
-	
-	public static final int ACQ_BACKWARD									= 6;
-	
-	public static final int FLYWHEEL_INCREASE							    = DriverStationControls.OP_XBOX_X;
-	
-	public static final int FLYWHEEL_DECREASE 								= DriverStationControls.OP_XBOX_B;
-	
-	public static final int AGITATOR_SERVO									= DriverStationControls.OP_XBOX_Y;
-
+	public static final int ACQ_ON										= DriverStationControls.OP_XBOX_POV_UP;
+	public static final int ACQ_OFF										= DriverStationControls.OP_XBOX_POV_DOWN;
+	public static final int ACQ_FEED_RIGHT								= DriverStationControls.OP_XBOX_POV_RIGHT;
+	public static final int ACQ_FEED_LEFT								= DriverStationControls.OP_XBOX_POV_LEFT;
 }
